a000c16591dce3a1cc299bf19f2697cc877ffb0c
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-02-26 19:33:58 +0530

    answer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UserDetails from './UserDetails';
+import UserList from './UserList';
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/user/:userId" element={<UserDetails />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
